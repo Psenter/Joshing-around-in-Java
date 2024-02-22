18b5d542b1eb8c29d14f9cebf5de80bd2cc25f72
Parker S <111610335+Psenter@users.noreply.github.com>
2024-02-22 08:00:54 -0500

    Added more text that can print.

--- goofing-off.java
+++ goofing-off.java
@@ -5,3 +5,28 @@ class mymessage {
         System.out.println("First print in Java.");
     }
 }
+
+class Tree {
+    public static void main(String[] args){
+      //This will print a tree to the terminal
+      System.out.println("I am going to plant a tree today!");
+      System.out.println("  *  ");
+      System.out.println(" *** ");
+      System.out.println(" *** ");
+      System.out.println("  *  ");
+      System.out.println("  *  ");
+      System.out.println("It is starting to grow!");
+      System.out.println("* ** *");
+      System.out.println("******");
+      System.out.println(" **** ");
+      System.out.println("  **  ");
+      System.out.println("  **  ");
+      System.out.println("It is fully grown now!");
+      System.out.println("*****");
+      System.out.println("*****");
+      System.out.println("*****");
+      System.out.println("  *  ");
+      System.out.println("  *  ");
+      System.out.println("######");
+    }
+  }
